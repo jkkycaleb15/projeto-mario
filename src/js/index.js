205ea11b69d5ac93f589e20b61b25fa442532228
jkkycaleb15 <123330012+jkkycaleb15@users.noreply.github.com>
2023-01-22 17:38:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+ 
+
+
+
+
+botaoTrailer.addEventListener("click", () =>  {
+     modal.classList.add("aberto");
+     video.setAttribute("src" , linkDoVideo);
+     
+});
+
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+console.log(botaoFecharModal);
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src" , "");
+
+    })
+
